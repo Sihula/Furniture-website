719cb539e7fbb6663eefc4d08ac2f24fc8942a72
Sihula <serhiisihula@gmail.com>
2021-11-12 12:17:33 +0100

    add right-side-bar

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+
+  $('.header__btn').on('click', function () {
+    $('.rightside-menu').removeClass('rihgtside-menu--close');
+
+  });
+
+  $('.rideside-close').on('click', function () {
+    $('.rightside-menu').addClass('rihgtside-menu--close');
+
+  });
+
+});
